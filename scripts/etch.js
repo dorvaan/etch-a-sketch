1350286e39cb7bb389d42442bf66fcde7870327c
Dustin Jones <coding.dustin@gmail.com>
2022-05-27 14:31:34 -0500

    Grid outline created

--- scripts/etch.js
+++ scripts/etch.js
@@ -2,7 +2,8 @@ const container = document.querySelector('.container');
 
 for (let i = 1; i < 257; i++) {
     const div = document.createElement('div');
-    div.style.cssText = "border: 1px solid black; height: 25px; width: 6%;";  
+    div.classList.add('grid')
+    div.style.cssText = "border: 1px solid black; width: 35.125px";  
     container.appendChild(div);
   }
 
