4e9b9834a7abda64b8b70af3b7614ca1b7a4d30c
Dustin Jones <coding.dustin@gmail.com>
2022-05-27 14:04:08 -0500

    Grid created working on maintaining size ratio

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,3 +1,13 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 
-const grid = document.getElementById
+for (let i = 1; i < 257; i++) {
+    const div = document.createElement('div');
+    div.style.cssText = "border: 1px solid black; height: 25px; width: 6%;";  
+    container.appendChild(div);
+  }
+
+
+// const grid = document.createElement('div');
+// grid.classList.add('grid');
+// grid.textContent = 'This is the glorious text-content!';
+// container.appendChild(grid);
