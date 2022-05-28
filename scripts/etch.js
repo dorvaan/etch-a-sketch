d34d5ba96a5168ff0b8e06391cd8ba5286cb64b9
Dustin Jones <coding.dustin@gmail.com>
2022-05-27 21:45:04 -0500

    Grid Color Change in progress

--- scripts/etch.js
+++ scripts/etch.js
@@ -4,19 +4,17 @@ const container = document.querySelector('.container');
 for (let i = 1; i < 257; i++) {
     const div = document.createElement('div');
     div.classList.add('grid')
-    div.style.cssText = "border: 1px solid black; width: 35.125px";  
+    div.style.cssText = "border: 1px solid black; width: 35.125px";
     container.appendChild(div);
-  }
-
-  //Color Grid
-let grid = document.getElementsByClassName("grid");
-
-grid.addEventListener("mouseover", fuction(gridColor), {
-    gridColor.target.style.color = "gray";
 }
 
+//Color Grid
 
-)
+let squares = document.getElementsByClassName("grid");
+
+squares.addEventListener("mouseover", function (event) {
+    alert("mouse over test!")
+}, false);
 
 
 // const grid = document.createElement('div');
