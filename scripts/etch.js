0bb419bb2caec6c1b2b4fb12b7cb084423b1ec56
Dustin Jones <coding.dustin@gmail.com>
2022-05-27 21:37:24 -0500

    Making grid change colors

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,11 +1,22 @@
 const container = document.querySelector('.container');
 
+// Build Grid
 for (let i = 1; i < 257; i++) {
     const div = document.createElement('div');
     div.classList.add('grid')
     div.style.cssText = "border: 1px solid black; width: 35.125px";  
     container.appendChild(div);
   }
+
+  //Color Grid
+let grid = document.getElementsByClassName("grid");
+
+grid.addEventListener("mouseover", fuction(gridColor), {
+    gridColor.target.style.color = "gray";
+}
+
+
+)
 
 
 // const grid = document.createElement('div');
