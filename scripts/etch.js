d14e48fcabf54d2be853045fbb7eacefdec9bc3c
Dustin Jones <coding.dustin@gmail.com>
2022-05-31 13:57:56 -0500

    Black grid color functioning

--- scripts/etch.js
+++ scripts/etch.js
@@ -5,11 +5,18 @@ for (let i = 1; i < 257; i++) {
     const div = document.createElement('div');
     div.classList.add('grid')
     div.style.cssText = "border: 1px solid black; width: 35.125px";
+    div.addEventListener("mouseover", function(color){
+       // div.style.cssText = "border: 1px solid black; width: 35.125px; background-color: black;"    
+       div.style.backgroundColor = "black";
+        })
     container.appendChild(div);
 }
 
+
+
+
 //Color Grid
 
-const grid = document.querySelector('.grid');
-grid.addEventListener("mouseenter", grid.style.cssText = "background-color: gray;");
+// const grid = document.querySelector('.grid');
+// grid.addEventListener("mouseenter", grid.style.cssText = "background-color: gray;");
 
